8c6f26b4274e8225c46c5b0024883f8d42f9c567
Bryan Bird <lebrygan@gmail.com>
2019-05-21 15:54:56 +0200

    Création des fichiers de bases

---
+++ view/welcomePage.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Mon blog</title>
+        <link href="public/style.css" rel="stylesheet" /> 
+    </head>
+        
+    <body>
+
+        <p>Welcome !</p>
+        
+    </body>
+</html>
