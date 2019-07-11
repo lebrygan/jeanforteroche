8707b37b36fc7c8e53284faa5ee036eafc7a739e
Bryan Bird <lebrygan@gmail.com>
2019-07-11 15:15:22 +0200

    Ajout de l'obligation d'entré un email pour l'ajout d'un utilisateur

--- js/manageUsers.js
+++ js/manageUsers.js
@@ -3,11 +3,15 @@ var addChangeUserButton = document.getElementById("addChangeUser");
 addChangeUserButton.addEventListener("click", (e)=>{
 
 	var formData = new FormData();
+	$email = document.getElementById("email").value;
 	$password = document.getElementById("password").value;
+	var mailRegex = /^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/i;
 	if($password.length < 2){
 		document.getElementById("respondUser").innerHTML = '<p class="alert alert-danger">Veuillez mettre un mot de passe</p>';
+	}else if(!mailRegex.test($email)){
+		document.getElementById("respondUser").innerHTML = '<p class="alert alert-danger">Veuillez mettre une adresse mail valide</p>';
 	}else{
-		formData.append("email", document.getElementById("email").value);
+		formData.append("email", $email);
 		formData.append("pwd", $password);
 
 		ajaxPost("http://cosmopoly.fr/controller/ajaxController/addChangeUsers.php",formData,
