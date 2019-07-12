fb2037d32d13fa310a202c6fb553d73dcbf546b7
Bryan Bird <lebrygan@gmail.com>
2019-07-12 11:34:28 +0200

    Corrections bug d'affichage

--- view/footer.php
+++ view/footer.php
@@ -1,17 +1,19 @@
 <?php ob_start(); ?>
 	<div class="container-fluid">
 		<div class="row">
-            <div class="col-md-6 col-sm-12 pb-2 bg-white">
+            <div class="col-md-6 col-sm-12 pb-2 mb-1 bg-white">
                 <h3 class="text-center">L'auteur</h3>
-                <div class="d-flex">
-                    <img id="authorImg" src="public/img/jean.jpg">
-                    <div class="ml-2">
-                    <h4>Jean Forteroche</h4>
-                    <p>Après des études scientifiques et un début de carrière dans la lutte contre la pollution, je “décide” (ma vie est tout à fait une suite planifiée de choix réfléchis) de me consacrer à ma famille et à l’écriture.</p>
+                <div class="row">
+                    <figure class="col-sm-5 col-xs-12">
+                        <img class="mw-100 mh-100" id="authorImg" src="public/img/jean.jpg" alt="Photos de l'auteur">
+                    </figure>
+                    <div class="pl-2 col-sm-6 col-xs-12">
+                        <h4>Jean Forteroche</h4>
+                        <p>Après des études scientifiques et un début de carrière dans la lutte contre la pollution, je “décide” (ma vie est tout à fait une suite planifiée de choix réfléchis) de me consacrer à ma famille et à l’écriture.</p>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-sm-12 p-0 align-items-left">
+            <div class="col-md-6 col-sm-12 p-0 mb-1 align-items-left">
                 <div class="bg-white ml-md-1 mt-md-0 mt-sm-1 pl-2 mb-0 h-100">
                     <h3 class="text-center">Contact</h3>
                     <h4 class="my-1">Email</h4>
