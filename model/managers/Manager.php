f3195ef5dc23e94ca08b78068a5b68aea7979818
Bryan Bird <lebrygan@gmail.com>
2019-07-15 10:51:55 +0200

    Modification fichier manager

--- model/managers/Manager.php
+++ model/managers/Manager.php
@@ -4,10 +4,10 @@ abstract class Manager
 {
 	protected function dbConnect()
 	{
-		$host_name = 'db5000124356.hosting-data.io';
+		$hostName = 'db5000124356.hosting-data.io';
 		$database = 'dbs118960';
-		$user_name = 'dbu26750';
-		$password = 'Cosmopoly12!';
+		$userName = ''; //Add your username
+		$password = ''; //Add your password
 		$dbName = 'dbs118960';
 	    try{
 	        $db = new PDO('mysql:host='.$host_name.';dbname='.$dbName.';charset=utf8', $user_name, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
